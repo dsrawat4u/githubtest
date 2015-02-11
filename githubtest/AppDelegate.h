c5cd824ced86d668c021a755ad3f87f3179aa83a
Harshita <enchanting.trance@gmail.com>
2015-02-11 11:38:33 +0530

    Initial Commit

---
+++ githubtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by spice on 11/02/15.
+//  Copyright (c) 2015 Spice Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ githubtest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by spice on 11/02/15.
+//  Copyright (c) 2015 Spice Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
